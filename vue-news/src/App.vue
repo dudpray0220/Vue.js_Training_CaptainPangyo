b53254eed90e8af88fc371cdcebf23c639fb5e85
YoungHyun Bae <qodudgus0220@naver.com>
2023-05-08 19:06:27 +0900

    first commit

---
+++ vue-news/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  methods: {
+    fetchData() {
+      console.log('hello');
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
